c37dbb2640ac88183cb2c6564d892079e3b184f0
Adam Szydło <adamszydlo@gmail.com>
2020-08-17 01:06:20 +0200

    More EC2 properties

--- hello-ec2.tf
+++ hello-ec2.tf
@@ -4,4 +4,7 @@ provider "aws" {
 resource "aws_instance" "example_17-08-2020" {
   ami   =  "ami-0389b2a3c4948b1a0"
   instance_type ="t2.micro"
+  name = "example_17-08-2020"
+  key_name = "London-KP"
+  vpc_security_group_ids = ["WebDMZ"]
 }

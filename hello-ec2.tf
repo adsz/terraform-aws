c17a96f912a1105047edf749f14872025111fa85
Adam Szydło <adamszydlo@gmail.com>
2020-08-17 01:17:14 +0200

    More EC2 properties v8

--- hello-ec2.tf
+++ hello-ec2.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = var.region
 }
 resource "aws_instance" "Dupa_Chuj_Cipa" {
-  environment = "test"
   ami   =  "ami-0389b2a3c4948b1a0"
   instance_type ="t2.micro"
   key_name = "London-KP"

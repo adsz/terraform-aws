1c503db06c3361d7d2e2f5dc3bd01b20ef331860
Adam Szydło <adamszydlo@gmail.com>
2020-08-17 01:10:01 +0200

    More EC2 properties v3

--- hello-ec2.tf
+++ hello-ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 resource "aws_instance" "example_17-08-2020" {
-  name = "example_17-08-2020"
+#  name = "example_17-08-2020"
   ami   =  "ami-0389b2a3c4948b1a0"
   instance_type ="t2.micro"
   key_name = "London-KP"

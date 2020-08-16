5f37220c9ed15b2b46a752519df633602cdde638
Adam Szydło <adamszydlo@gmail.com>
2020-08-17 00:19:09 +0200

    Added AWS profile name

--- hello-ec2.tf
+++ hello-ec2.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region = "eu-west-2"
   shared_credentials_file = "/home/adam/.aws/credentials"
+  profile = "default"
 }
 resource "aws_instance" "example_17-08-2020" {
   ami   =  "ami-0389b2a3c4948b1a0"

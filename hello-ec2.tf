88da46337081aa92f98209c760e5adcbf27784fd
Adam Szydło <adamszydlo@gmail.com>
2020-08-17 00:15:32 +0200

    removed illegal space char

--- hello-ec2.tf
+++ hello-ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-west-2"
   shared_credentials_file = "/home/adam/.aws/credentials"
 }
-resource "aws_instance" "example 17-08-2020" {
+resource "aws_instance" "example_17-08-2020" {
   ami   =  "ami-0389b2a3c4948b1a0"
   instance_type ="t2.micro"
 }

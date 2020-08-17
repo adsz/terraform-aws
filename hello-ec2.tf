7bd9091e083eafca3e847df8fd12b6ea04aef0a4
Adam Szydło <adamszydlo@gmail.com>
2020-08-17 02:28:13 +0200

    More EC2 properties v14

--- hello-ec2.tf
+++ hello-ec2.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region = var.region
 }
-resource "aws_instance" "Dupa_Chuj_Cipa" {
+resource "aws_instance" "Dupa_Chuj_Cipa_2" {
   ami = "ami-0389b2a3c4948b1a0"
   instance_type = "t2.micro"
   key_name = "London-KP"
   vpc_security_group_ids = ["sg-06762a03e15c74b8b"]
-  #tags = {
-  #  Name = "Terraform test EC2 v4"
-  #  Environment = "test"
-  #  }
+  tags = {
+    Name = "Terraform test EC2 v4"
+    Environment = "test"
+    }
 }

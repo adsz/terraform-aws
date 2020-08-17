fff9881cb7068cea9d9cb6a3f36e862edf950f24
Adam Szydło <adamszydlo@gmail.com>
2020-08-17 02:10:56 +0200

    More EC2 properties v12

--- hello-ec2.tf
+++ hello-ec2.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "Dupa_Chuj_Cipa" {
   instance_type = "t2.micro"
   key_name = "London-KP"
   vpc_security_group_ids = ["sg-0a7e4fe4d0a075489"]
-  tags = {
-    Name = "Terraform test EC2 v3"
-    Environment = "test"
-    }
+  #tags = {
+  #  Name = "Terraform test EC2 v3"
+  #  Environment = "test"
+  #  }
 }

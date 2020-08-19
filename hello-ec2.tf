19ad00b466b079e73b09f403499695fd0aeff09d
Adam Szydło <adamszydlo@gmail.com>
2020-08-20 00:54:55 +0200

    More EC2 properties v9

--- hello-ec2.tf
+++ hello-ec2.tf
@@ -11,3 +11,13 @@ resource "aws_instance" "Dupa_Chuj_Cipa_2" {
     Environment = "test"
     }
 }
+resource "aws_instance" "Ubuntu" {
+  ami = "ami-0389b2a3c4948b1a0"
+  instance_type = "t2.micro"
+  key_name = "London-KP"
+  vpc_security_group_ids = ["sg-06762a03e15c74b8b"]
+  tags = {
+    Name = "Ubuntu worker"
+    Environment = "test"
+    }
+}
